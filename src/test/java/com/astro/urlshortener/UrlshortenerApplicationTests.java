1121dd0cece345863e0b38a7e860a2c21dcceb38
astrophilic <pankajhz99@gmail.com>
2023-04-23 19:03:48 +0530

    starting urlShortnerApplication

---
+++ src/test/java/com/astro/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
