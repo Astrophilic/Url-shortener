5c1a882f82ced584352c6a270aff971d85ead323
astrophilic <pankajhz99@gmail.com>
2023-04-24 04:11:21 +0530

    refactor to add new Method and make restController cleaner

--- src/main/java/com/astro/urlshortener/service/UrlService.java
+++ src/main/java/com/astro/urlshortener/service/UrlService.java
@@ -16,36 +16,52 @@ public class UrlService {
     @Autowired
     private UrlRepository urlRepository;
 
-    private static String toBase64(Long id){
+    private static String toBase64(Long id) {
 
         // Convert the integer to base 64
         StringBuilder sb = new StringBuilder();
         while (id > 0) {
             long remainder = id % 64;
-            sb.append(base64Chars.charAt((int)remainder));
+            sb.append(base64Chars.charAt((int) remainder));
             id /= 64;
         }
 
         // Reverse the base 64 string and return it
         return sb.reverse().toString();
     }
-    public String generateShortUrl(final Long id){
+
+    public String generateShortUrl(final Long id) {
         // encode the long url in base64
         String encodedUrl = toBase64(id);
 
-        int appendTimes= (encodedUrl.length()<7?7-encodedUrl.length():0);
+        int appendTimes = (encodedUrl.length() < 7 ? 7 - encodedUrl.length() : 0);
 
-        String shortUrl = BASE_URL+encodedUrl+"=".repeat(appendTimes);
+        String shortUrl = BASE_URL + encodedUrl + "=".repeat(appendTimes);
         return shortUrl;
-
 
     }
 
-    public Optional<URL> findUrlbyShortUrl(final String shortUrl){
+    public String generateNewUrl(final String longUrl) {
+        String shortUrl = "";
+        URL newUrl = new URL();
+        newUrl.setLongUrl(longUrl);
+        newUrl.setShortUrl(longUrl);
+        saveUrl(newUrl);
+        Optional<URL> currentUrl = findUrlByLongUrl(longUrl);
+        if (currentUrl.isPresent()) {
+            Long id = currentUrl.get().getId();
+            shortUrl = generateShortUrl(id);
+            currentUrl.get().setShortUrl(shortUrl);
+            saveUrl(currentUrl.get());
+        }
+        return shortUrl;
+    }
+
+    public Optional<URL> findUrlbyShortUrl(final String shortUrl) {
         return urlRepository.findURLByShortUrl(shortUrl);
     }
 
-    public URL saveUrl(final URL url){
+    public URL saveUrl(final URL url) {
         return urlRepository.save(url);
     }
 
