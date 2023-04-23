d545aec02312d8ed5d65df9634cf8f90071bdb23
astrophilic <pankajhz99@gmail.com>
2023-04-24 04:02:14 +0530

    adding URL model

--- src/main/java/com/astro/urlshortener/model/URL.java
+++ src/main/java/com/astro/urlshortener/model/URL.java
@@ -9,6 +9,7 @@ import jakarta.persistence.*;
  * String shortUrl: can't be null, and generated using UrlService
  */
 @Entity
+@Table
 public class URL {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +17,30 @@ public class URL {
 
     @Column(nullable = false,unique = true)
     private String longUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
 
     @Column(nullable = false,unique = true)
     private String shortUrl;
